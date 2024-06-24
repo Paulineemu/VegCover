2081e2242ac35f8a56d37b044a68d5bb5edaff0a
Paulineemu <162981115+Paulineemu@users.noreply.github.com>
2024-06-24 14:52:18 +0200

    Update script.py without imported train function

--- script.py
+++ script.py
@@ -8,35 +8,65 @@ import pandas as pd
 import shutil
 import random
 
+# import function to predict on raw images and estimate percentage cover per species 
+from functions import predict 
+
 # ******************************************************************************
 ### Train frame detegtion model ### 
 # path to training data for frame detection model 
 path_to_data_frame="path_to/project_location" # path to the root where you have the yaml and the images and labels subfolder
 
-# train frame detegtion model 
-trained_model_path_frame = train_yolo(path_to_data_frame, task=detect, mode=train, im_size=640, n_epochs=50, batch_size=4, model='yolov8n.pt')
+# Structure of the folder for training the model
 
-# import function to train yolo model
-from functions import train_yolo
+-root
+      -images
+              -img_1.jpeg
+              -img_2.jpeg
+              -...
+      -labels
+              -img_1.txt
+              -img_2.txt
+              -...
+      -dataset.yaml
+              -names:
+                0: class1
+                1: class2
+                ...
+              -path: /content/data/project_title
+              -train: train.txt
+              -val: val.txt
+      -train.txt
+              -./images/img_1.jpg
+      -val.txt
+              -./images/img_2.jpg
 
-# call function 
-trained_model_path_frame = train_yolo(path_to_data_frame) 
+# Set YOLO parameters
+IM_SIZE = 640 # Image size for training
+N_EPOCHS = 50 # Number of epochs for training
+BATCH_SIZE = 4 # Batch size for training
+MODEL = 'yolov8n.pt' # Choose pretrained YOLO model version and size
+
+# Train frame detection model
+!yolo task=detect mode=train model=$MODEL data="{path_to_data_frame}/dataset.yaml", epochs=$N_EPOCHS, imgsz=$IM_SIZE, batch=$BATCH_SIZE, project=path_to_data_frame, name="species_detector_imgsz"+str(IM_SIZE)+"_modelSizeN"
+
+# Construct the path to the trained model's best weights
+trained_model_path_frame = f'{path_to_data_frame}/species_detector_imgsz{IM_SIZE}_modelSizeN/weights/best.pt'
 
 # ******************************************************************************
 ### Train species segmentation model ###
 # path to training data for species segmentation model 
 path_to_data_species="path_to/project_location" # path to the root where you have the yaml and the images and labels subfolder
 
-# train species segmentation model 
-trained_model_path_species = train_yolo(path_to_data_species, mode=segment, task=detect, mode=train, im_size=640, n_epochs=50, batch_size=4, model='yolov8n.pt'))
+# Train species segmentation model
+!yolo task=segment mode=train model=$MODEL data="{path_to_data_species}/dataset.yaml", epochs=$N_EPOCHS, imgsz=$IM_SIZE, batch=$BATCH_SIZE, project=path_to_data_species, name="species_detector_imgsz"+str(IM_SIZE)+"_modelSizeN"
+
+# Construct the path to the trained model's best weights
+trained_model_path_species = f'{path_to_data_species}/species_detector_imgsz{IM_SIZE}_modelSizeN/weights/best.pt'
 
 # ******************************************************************************
 ### Predict percentage coverage per species ###
 # path to raw images 
 path_to_data_images = "path_to/images" # path to the folder containing the images taking in the field
-
-# import function to predict on raw images and estimate percentage cover per species 
-from functions import predict 
 
 # predict on raw images 
 predict(path_to_data_images, path_to_model_frame=trained_model_path_frame, name_frame_class="frame", # parameter for cropping to frame
